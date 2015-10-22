71a16f659899b11fd2607c2a8b35d32a4b9fa04d
Matt Pedersen <mpedersen2054@gmail.com>
2015-10-22 00:35:28 -0500

    add business logic for ko stage, still need to implement jquery widget and the display logic for ko stage

--- src/javascripts/worlds.js
+++ src/javascripts/worlds.js
@@ -36,12 +36,10 @@ worlds.teams = [
 // for knockout stage
 worlds.ko = {};
 worlds.ko.qfinal = [];
-worlds.ko.sfinal = [];
-worlds.ko.gfinal = [];
 
-// add teams to KO stage by
-// seeds : 1, 8, 4, 5, 2, 7, 3, 6
-function addTeamToStage(acro, stage, seed) {
+// add teams to KO stage & add properties
+// seeds : 1 vs 8, 4 vs 5, 2 vs 7, 3 vs 6
+function addTeamToKnockout(acro, stage, seed) {
   var team = _.clone(worlds.teams[_.findIndex(worlds.teams, { acro: acro })]);
   team.koseed = seed;
   team.knockoutStage = {
@@ -54,48 +52,56 @@ function addTeamToStage(acro, stage, seed) {
 }
 
 // double elimination bracket according to lcs.bracket
-addTeamToStage('YOE', 'qfinal', 1);
-addTeamToStage('FNC', 'qfinal', 2);
-addTeamToStage('KT', 'qfinal', 3);
-addTeamToStage('SKT', 'qfinal', 4);
-addTeamToStage('AHQ', 'qfinal', 5);
-addTeamToStage('KOO', 'qfinal', 6);
-addTeamToStage('EDG', 'qfinal', 7);
-addTeamToStage('OG', 'qfinal', 8);
+addTeamToKnockout('YOE', 'qfinal', 1);
+addTeamToKnockout('FNC', 'qfinal', 2);
+addTeamToKnockout('KT', 'qfinal', 3);
+addTeamToKnockout('SKT', 'qfinal', 4);
+addTeamToKnockout('AHQ', 'qfinal', 5);
+addTeamToKnockout('KOO', 'qfinal', 6);
+addTeamToKnockout('EDG', 'qfinal', 7);
+addTeamToKnockout('OG', 'qfinal', 8);
 
-console.log(worlds.ko.qfinal)
-
-function playGame(t1id, t2id, outcome, victorid) {
+function playGame(t1seed, t2seed, outcome, victorid, nextStage) {
   // tXs is team1 seed & team2 seed
   // outcome = { x: 3, y: 1 };
-  var team1 = worlds.ko.qfinal[_.findIndex(worlds.ko.qfinal, { koseed: t1id })];
-  var team2 = worlds.ko.qfinal[_.findIndex(worlds.ko.qfinal, { koseed: t2id })];
-  console.log(team1, team2);
+  var team1 = worlds.ko.qfinal[_.findIndex(worlds.ko.qfinal, { koseed: t1seed })];
+  var team2 = worlds.ko.qfinal[_.findIndex(worlds.ko.qfinal, { koseed: t2seed })];
 
-  if (victorid === t1id) {
+  if (victorid === t1seed) {
     team1.knockoutStage.record.w += outcome.w;
     team1.knockoutStage.record.l += outcome.l;
     team2.knockoutStage.record.w += outcome.l;
     team2.knockoutStage.record.l += outcome.w;
+    team1.knockoutStage[nextStage] = true;
   }
-  else if (victorid === t2id) {
+  else if (victorid === t2seed) {
     team2.knockoutStage.record.w += outcome.w;
     team2.knockoutStage.record.l += outcome.l;
     team1.knockoutStage.record.w += outcome.l;
     team1.knockoutStage.record.l += outcome.w;
+    team2.knockoutStage[nextStage] = true;
   }
   else {
     console.log('something went wrong');
   }
-
 }
 
-playGame(1, 8, { w: 3, l: 1 }, 8);
-console.log(worlds.ko.qfinal)
+// SEMIFINALS
+// seeds : 1 vs 8, 4 vs 5, 2 vs 7, 3 vs 6
+playGame(1, 8, { w: 3, l: 1 }, 8, 'sfinal');
+playGame(4, 5, { w: 3, l: 0 }, 4, 'sfinal');
+playGame(2, 7, { w: 3, l: 0 }, 2, 'sfinal');
+playGame(3, 6, { w: 3, l: 1 }, 6, 'sfinal');
+// filter only teams whose sfinal = true
+var sfinal = _.filter(worlds.ko.qfinal, function(team) {
+  return team.knockoutStage.sfinal == true;
+});
 
-// SEMIFINAL GAME1
 
-
+// SEMIFINAL ( implement sat/sun )
+worlds.ko.sfinal = sfinal;
+// playGame(1, 8, { w: 3, l: 1 }, 8, 'sfinal');
+// playGame(1, 8, { w: 3, l: 1 }, 8, 'sfinal');
 
 
 // set groups and call appendHTML
